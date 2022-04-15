a82ce2f4b5011cb8b9be0adc00f1bd4a5255d519
Volkan Karakus <vkarakus@etu.edu.tr>
2022-04-15 14:54:36 +0300

    architectural arrangement

--- flyingdonkeylasttask/lib/main.dart
+++ flyingdonkeylasttask/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flyingdonkeylasttask/HomeScreen.dart';
+import 'package:flyingdonkeylasttask/page/HomeScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
 
